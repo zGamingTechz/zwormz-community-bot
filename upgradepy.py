d76a0f1dea312bcbf150d157a5b4b06724ac24b0
Bhavya Soni <rkumarbhavy@gmail.com>
2025-05-05 01:09:41 +0530

    Removed extra files

--- upgradepy.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Run shell commands to upgrade Python using apt
-os.system("apt update")
-os.system("apt upgrade python3")
-
-# Check the new Python version after the upgrade
-os.system("python3 --version")
